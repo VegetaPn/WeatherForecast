fd76d8300fcfe175a087ac86f9e8889592b89426
邢金涛 <13301025@bjtu.edu.cn>
2015-07-15 11:10:33 +0800

    修改增加城市页面控件 改为listview

--- gen/weatherforecast/view/R.java
+++ gen/weatherforecast/view/R.java
@@ -24,13 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
         public static final int addCityL1=0x7f080007;
         public static final int addCityL2=0x7f080006;
         public static final int btn_addCity=0x7f080003;
         public static final int btn_location=0x7f080004;
         public static final int btn_searchWeather=0x7f080002;
         public static final int editTextInputCityName=0x7f080001;
+        public static final int listViewshowcity=0x7f080008;
         public static final int textViewShowMessage=0x7f080005;
         public static final int textViewTipForInputCityName=0x7f080000;
     }
@@ -38,6 +39,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int add_city=0x7f030001;
         public static final int succeed=0x7f030002;
+        public static final int viewitem=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
