f767ff02591b4e315d68711257e8d937757e6240
邢金涛 <13301025@bjtu.edu.cn>
2015-07-22 15:50:38 +0800

    4*3插件 粗略布局版

---
+++ src/weatherforecast/view/WeatherWidgetB.java
@@ -0,0 +1,17 @@
+package weatherforecast.view;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class WeatherWidgetB extends AppWidgetProvider{
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		// TODO Auto-generated method stub
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		
+	}
+
+}
